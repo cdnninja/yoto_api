71240b18bc2175e8510ab041a17895664ef34c5b
cdnninja <jaydenaphillips@gmail.com>
2024-04-13 10:46:01 -0600

    Update yotoManager.py

--- yoto_api/yotoManager.py
+++ yoto_api/yotoManager.py
@@ -7,6 +7,10 @@ class YotoManager:
         self.username: str = username
         self.password: str = password
         self.api: YotoAPI = YotoAPI(self)
+        self.players: dict = {}
+        self.token: GetToken = None
 
     def initialize(self) -> None:
         self.token: GetToken = self.YotoAPI.login(self.username, self.password)
+        self.players = self.api.get_devices(self.token)
+
