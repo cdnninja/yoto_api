1a2babf225e89422506313d08b4f93705f963904
cdnninja <jaydenaphillips@gmail.com>
2024-04-23 08:10:00 -0600

    fix: Add another light value

--- yoto_api/const.py
+++ yoto_api/const.py
@@ -4,11 +4,13 @@ DOMAIN: str = "yoto_api"
 
 # Blue night_light_mode 0x194a55
 # off is 0x000000
+# 0x643600 is a valid response too. I think this is day. 
 
 LIGHT_COLORS = {
     None: None,
     "0x000000": "Off",
-    "0x194a55": "On"
+    "0x194a55": "On Night",
+    "0x643600": "On Day"
 }
 
 POWER_SOURCE = {
