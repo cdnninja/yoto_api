d5309c61febc164365f50ab949c04236a7941019
GitHub Action <action@github.com>
2024-05-22 01:39:18 +0000

    chore: bumping version to 1.15.11

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cdnninja/yoto_api",
-    version="1.15.10",
+    version="1.15.11",
     zip_safe=False,
 )
