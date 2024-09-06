3d7610f2baa9e9bfa3bb5df1b427f3a6f9e03487
cdnninja <jaydenaphillips@gmail.com>
2024-09-06 15:46:31 -0600

    fix: unsupported temp (#90)

--- yoto_api/YotoAPI.py
+++ yoto_api/YotoAPI.py
@@ -125,7 +125,10 @@ class YotoAPI:
             players[deviceId].system_volume = get_child_value(
                 player_status_response, "systemVolumePercentage"
             )
-            if get_child_value(player_status_response, "temperatureCelcius").isdigit:
+            if (
+                get_child_value(player_status_response, "temperatureCelcius")
+                != "notSupported"
+            ):
                 if (
                     int(get_child_value(player_status_response, "temperatureCelcius"))
                     != 0
