e79e2dc89dfbe4653e98520e0bd29663d6a92632
cdnninja <jaydenaphillips@gmail.com>
2025-04-18 14:05:30 -0600

    feat: authentication error exception

---
+++ yoto_api/exceptions.py
@@ -0,0 +1,15 @@
+"""exceptions.py"""
+
+class YotoException(Exception):
+    """
+    Generic YotoException exception.
+    """
+
+    pass
+
+class AuthenticationError(YotoException):
+    """
+    Raised upon receipt of an authentication error.
+    """
+
+    pass
